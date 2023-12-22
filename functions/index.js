6aa5c60c205bedca9f18cefd324cff5e93946094
Luwam Mogos <luwam.mogosw@gmail.com>
2023-12-21 22:31:44 -0700

    fifth

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Import function triggers from their respective submodules:
  *
  * const {onCall} = require("firebase-functions/v2/https");
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-/*const {onRequest} = require("firebase-functions/v2/https");
+/* const {onRequest} = require("firebase-functions/v2/https");
 //const logger = require("firebase-functions/logger");*/
 
 // Create and deploy your first functions
